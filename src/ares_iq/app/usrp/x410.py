b71a75367a64cdca9c0287b9fa1060a95bb96d09
Tom Schmitz <tschmitz@andrew.cmu.edu>
2025-09-30 18:37:49 -0400

    Remove usrp platforms for now

--- src/ares_iq/app/usrp/x410.py
+++ src/ares_iq/app/usrp/x410.py
@@ -1,10 +1,10 @@
-from .usrp import UsrpDevice
-
-
-class X410Device(UsrpDevice):
-    def _quantize(self):
-        pass
-
-    @property
-    def type(self):
-        return "x400"
+# from .usrp import UsrpDevice
+#
+#
+# class X410Device(UsrpDevice):
+#     def _quantize(self):
+#         pass
+#
+#     @property
+#     def type(self):
+#         return "x400"
