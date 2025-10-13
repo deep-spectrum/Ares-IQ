5626d6c86d7e300911d001ea411e48fb5109b352
Tom Schmitz <tschmitz@andrew.cmu.edu>
2025-10-13 17:00:50 -0400

    Update x310 platform

--- src/ares_iq/app/usrp/__init__.py
+++ src/ares_iq/app/usrp/__init__.py
@@ -1,9 +1,9 @@
 # from .x410 import X410Device
-# from .x310 import X310Device
-# from ares_iq.typing import SoftwareDefinedRadio
-#
-#
-# PLATFORMS: dict[str, SoftwareDefinedRadio] = {
+from .x310 import X310Device
+from ares_iq.typing import SoftwareDefinedRadio
+
+
+PLATFORMS: dict[str, SoftwareDefinedRadio] = {
 #     "x410": X410Device(),
-#     "x310": X310Device(),
-# }
+    "x310": X310Device(),
+}
--- src/ares_iq/app/usrp/x310.py
+++ src/ares_iq/app/usrp/x310.py
@@ -1,10 +1,12 @@
-# from .usrp import UsrpDevice
-#
-#
-# class X310Device(UsrpDevice):
-#     def _quantize(self):
-#         pass
-#
-#     @property
-#     def type(self):
-#         return "x300"
+from .usrp import USRP
+from ares_iq_ext.usrp import _USRPConfigs
+
+
+class X310Device(USRP):
+    def _quantize(self):
+        pass
+
+    def __init__(self):
+        configs = _USRPConfigs()
+        configs.dev_args = 'type=x300'
+        super().__init__(configs)
