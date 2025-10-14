c700ac9661278e124ec432dc16a713ecd527f06b
Tom Schmitz <tschmitz@andrew.cmu.edu>
2025-10-14 16:43:08 -0400

    Add way to hide progress bar in Python

--- src/ares_iq/print_utils/progress_bars.py
+++ src/ares_iq/print_utils/progress_bars.py
@@ -3,7 +3,11 @@ import datetime as dt
 
 
 class CaptureProgress:
-    def __init__(self, captures: int, samples_per_capture: int):
+    def __init__(self, captures: int, samples_per_capture: int, hide: bool = False):
+        if hide:
+            self._hide = True
+            return
+        self._hide = False
         self._samples_captured = 0
         self._samples_per_capture = samples_per_capture
         self._progress = Progress(TextColumn("Capturing..."),
@@ -15,14 +19,18 @@ class CaptureProgress:
         self._start = None
 
     def __enter__(self):
-        self._progress.start()
-        self._start = dt.datetime.now()
+        if not self._hide:
+            self._progress.start()
+            self._start = dt.datetime.now()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self._progress.stop()
+        if not self._hide:
+            self._progress.stop()
 
     def update(self):
+        if self._hide:
+            return
         self._samples_captured += self._samples_per_capture
         time_diff = (dt.datetime.now() - self._start).total_seconds()
         rate = f"{self._samples_captured / time_diff / 1e6:.2f} megasamples/second"
