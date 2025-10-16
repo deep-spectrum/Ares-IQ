adad2f105f10c9346cdcbec04e67bbcb72a78ee1
Tom Schmitz <tschmitz@andrew.cmu.edu>
2025-10-16 16:26:21 -0400

    Update protocol naming

--- src/ares_iq/typing.py
+++ src/ares_iq/typing.py
@@ -3,14 +3,14 @@ from .iq_data import IQData
 
 
 class SoftwareDefinedRadio(Protocol):
-    def capture_iq(self, center: float, bw: float, file_size: float, verbose: bool, extra: bool) -> None:
+    def capture_iq(self, center: float, bw: float, file_size: float, verbose: bool, extra_verbose: bool) -> None:
         """
         Capture IQ data from the SDR.
         :param center: The center frequency in Hz
         :param bw: The bandwidth in Hz
         :param file_size: The maximum amount of IQ data to collect in GB
-        :param verbose: Verbose output
-        :param extra: Extra verbose output
+        :param verbose: Show progress bar.
+        :param extra_verbose: Extra verbose output. Shows the progress bar and logging messages.
         :return: The captured IQ data and the
         """
 
