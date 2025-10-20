2919862f8e3ef0a11d1a65cefa1977b4ffef8de6
Tom Schmitz <tschmitz@andrew.cmu.edu>
2025-10-20 17:42:14 -0400

    Now setting stream arguments in init

--- src/ares_iq/usrp/usrp.py
+++ src/ares_iq/usrp/usrp.py
@@ -1,4 +1,4 @@
-from ares_iq_ext.usrp import _USRP, _USRPConfigs
+from ares_iq_ext.usrp import _USRP, _USRPConfigs, _UsrpStreamArgs
 from ares_iq.iq_data import IQData
 from decimal import Decimal
 from abc import abstractmethod, ABC
@@ -11,13 +11,16 @@ class USRPConfigs:
                  subdev: str = "A:0",
                  ref: str = "internal",
                  rate: int | float = 25e6,
-                 gain: int | float = 0):
+                 gain: int | float = 0,
+                 samples_per_packet: int = 200):
         self._configs = _USRPConfigs()
+        self._stream_args = _UsrpStreamArgs()
         self._configs.samples_per_capture = samples_per_capture
         self._configs.subdev = subdev
         self._configs.ref = ref
         self._configs.rate = rate
         self._configs.gain = gain
+        self._stream_args.spp = samples_per_packet
 
     @property
     def samples_per_capture(self):
@@ -59,20 +62,31 @@ class USRPConfigs:
     def gain(self, k: int | float):
         self._configs.gain = k
 
+    @property
+    def samples_per_packet(self):
+        return self._stream_args.spp
+
+    @samples_per_packet.setter
+    def samples_per_packet(self, spp: int):
+        self._stream_args.spp = spp
+
+    @property
+    def configs_(self):
+        return self._configs
+
+    @property
+    def stream_args_(self):
+        return self._stream_args
+
 
 class USRP(ABC):
     _iq_data: list[IQData]
     _quantized_data: list[None]
 
-    def __init__(self, configs: _USRPConfigs):
-        self._usrp: _USRP = _USRP(configs)
-
-    @abstractmethod
-    def _stream_args(self):
-        pass
+    def __init__(self, configs: _USRPConfigs, stream_args: _UsrpStreamArgs):
+        self._usrp: _USRP = _USRP(configs, stream_args)
 
     def capture_iq(self, center: float, bw: float, file_size: float, verbose: bool, extra: bool):
-        self._stream_args()
         try:
             iq_data, timestamps = self._usrp.capture_iq(center, bw, file_size, verbose, extra)
         except ValueError as e:
