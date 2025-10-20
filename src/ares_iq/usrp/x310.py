17585a429437324a72f2764a1ef8fd4ea3e93936
Tom Schmitz <tschmitz@andrew.cmu.edu>
2025-10-20 17:49:54 -0400

    Made the x310 wrapper more simple

--- src/ares_iq/usrp/x310.py
+++ src/ares_iq/usrp/x310.py
@@ -1,53 +1,12 @@
 from .usrp import USRP, USRPConfigs
 from ares_iq_ext.usrp import _USRPConfigs, _UsrpStreamArgs
-from ares_iq.configurations import load_config_section
 
 
 class UsrpX310(USRP):
-    _from_cli = False
-
-    def _load_configs(self):
-        configs = load_config_section('x310-configs')
-        configs_ = _USRPConfigs()
-        configs_.dev_args = "type=x300"
-
-        if not self._from_cli:
-            return configs_
-
-        if "spc" in configs:
-            configs_.samples_per_capture = int(configs["spc"])
-
-        if "subdev" in configs:
-            configs_.subdev = configs["subdev"]
-
-        if "ref" in configs:
-            configs_.ref = configs["ref"]
-
-        if "rate" in configs:
-            configs_.rate = float(configs["rate"])
-
-        if "gain" in configs:
-            configs_.gain = float(configs["gain"])
-
-        return configs_
-
-    def _load_stream_args(self):
-        configs = load_config_section("x310-stream-configs")
-        configs_ = _UsrpStreamArgs()
-
-        if not self._from_cli:
-            return configs_
-
-        if "spp" in configs:
-            configs_.spp = int(configs["spp"])
-
-        return configs_
-
     def __init__(self, configs: USRPConfigs | None = None):
-        self._spp = None
         if configs is None:
-            configs_ = self._load_configs()
-            stream_args_ = self._load_stream_args()
+            configs_ = _USRPConfigs()
+            stream_args_ = _UsrpStreamArgs()
         else:
             configs_ = configs.configs_
             configs_.dev_args = "type=x300"
