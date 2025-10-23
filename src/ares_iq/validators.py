d47be31881f322e9068ecfc61dd109c8985b145e
Tom Schmitz <tschmitz@andrew.cmu.edu>
2025-10-23 15:32:01 -0400

    Add a validator to check if number is positive

--- src/ares_iq/validators.py
+++ src/ares_iq/validators.py
@@ -74,3 +74,40 @@ def power_of_two(_instance: Any, attribute: Attribute, value: int):
     """
     if not ((value & (value - 1) == 0) and value > 0):
         raise ValueError(f"{attribute.name} must be a power of 2")
+
+
+def is_positive(_instance: Any, attribute: Attribute, value: RealNumber):
+    """Validates if the input is positive.
+
+    Validates if the input value is valid. If `0` is considered valid, then
+    that must be specified in the metadata with the `"zero_valid"` key. If
+    `0` is considered to be invalid, then specifying metadata is not necessary.
+
+    Typical usage example:
+
+    ```py
+    from attrs import define, field
+    from ares_iq.validators import is_positive
+    @define
+    class Foo:
+        bar: int field(default=1,
+                       metadata={"zero_valid": False},  # Not necessary if `0` is considered invalid.
+                       validator=is_positive)
+    ```
+
+    Args:
+        _instance: Unused
+        attribute: The attribute being set
+        value: The value to validate
+
+    Raises:
+        ValueError: Value is not a positive number.
+    """
+    zero_valid = False
+    if attribute.metadata is not None:
+        if "zero_valid" in attribute.metadata:
+            zero_valid = bool(attribute.metadata["zero_valid"])
+
+    if value < 0 or (value == 0 and not zero_valid):
+        raise ValueError(
+            f"{attribute.name} must be greater than 0" if not zero_valid else f"{attribute.name} must be greater or equal to 0")
