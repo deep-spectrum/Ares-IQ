3f66dc1a988b97f53dcd80ff807c9d912b684d9e
Tom Schmitz <thomas.schmitz98@outlook.com>
2025-10-30 17:45:59 -0400

    Docstrings

--- src/ares_iq/usrp/x310.py
+++ src/ares_iq/usrp/x310.py
@@ -5,6 +5,7 @@ from enum import Enum
 
 
 class X310RefOptions(Enum):
+    """Reference clock options for the USRP x310."""
     INTERNAL = "internal"
     EXTERNAL = "external"
     GPSDO = "gpsdo"
@@ -15,6 +16,7 @@ class X310RefOptions(Enum):
 
 @define
 class X310Configs(USRPConfigs):
+    """Configurations for the USRP x310 platform."""
     _ref_options: tuple[str] = field(default=tuple([str(x) for x in X310RefOptions]), init=False, repr=False)
 
 
