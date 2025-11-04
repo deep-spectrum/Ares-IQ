e460d2aea0b6b968356ff913626d3a295e748c68
Tom Schmitz <thomas.schmitz98@outlook.com>
2025-11-03 19:23:08 -0500

    Update logging handle to use new console print functions

--- src/ares_iq/print_utils/logging.py
+++ src/ares_iq/print_utils/logging.py
@@ -1,38 +1,37 @@
 import logging
-from .console_print import print_error, print_warning
-from rich import print as rprint
-
-
-def _dbg_print(msg: str):
-    rprint(f"[DBG] {msg}")
-
-
-def _info_print(msg: str):
-    rprint(f"[green][INFO][/green] {msg}")
-
-
-def _crit_print(msg: str):
-    rprint(f"[][CRITICAL][/] {msg}")
+from .console_print import print_debug, print_info, print_warning, print_error, print_critical
 
 
 class AresIqLoggingHandle(logging.Handler):
-    def __init__(self, level: int | str = 0):
+    def __init__(self,
+                 level: int | str = 0,
+                 dbg_panel: bool = False,
+                 info_panel: bool = False,
+                 warning_panel: bool = False,
+                 error_panel: bool = False,
+                 critical_error_panel: bool = False):
         super().__init__(level=level)
 
         formatter = logging.Formatter("%(message)s")
         self.setFormatter(formatter)
+
+        self._dbg_panel = dbg_panel
+        self._inf_panel = info_panel
+        self._wrn_panel = warning_panel
+        self._err_panel = error_panel
+        self._crit_panel = critical_error_panel
 
     def emit(self, record: logging.LogRecord):
         msg = self.format(record)
 
         match record.levelno:
             case logging.DEBUG:
-                _dbg_print(msg)
+                print_debug(msg, self._dbg_panel)
             case logging.INFO:
-                _info_print(msg)
+                print_info(msg, self._inf_panel)
             case logging.WARNING:
-                print_warning(msg)
+                print_warning(msg, self._wrn_panel)
             case logging.ERROR:
-                print_error(msg)
+                print_error(msg, self._err_panel)
             case logging.CRITICAL:
-                _crit_print(msg)
+                print_critical(msg, self._crit_panel)
