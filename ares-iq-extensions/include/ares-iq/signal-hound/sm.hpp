f71f33ff3e96fe49bcf3ff79657ee63ffd90f603
Tom Schmitz <thomas.schmitz98@outlook.com>
2025-11-05 18:08:21 -0500

    Add signal hound files

---
+++ ares-iq-extensions/include/ares-iq/signal-hound/sm.hpp
@@ -0,0 +1,10 @@
+//
+// Created by tschmitz on 11/5/25.
+//
+
+#ifndef VERSION_SM_HPP
+#define VERSION_SM_HPP
+
+
+
+#endif //VERSION_SM_HPP
---
+++ ares-iq-extensions/src/signal-hound/sm_series.cpp
@@ -0,0 +1,9 @@
+//
+// Created by tschmitz on 11/5/25.
+//
+
+#include <ares-iq/signal-hound/sm/sm_api.h>
+#include <ares-iq/signal-hound/sm.hpp>
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
