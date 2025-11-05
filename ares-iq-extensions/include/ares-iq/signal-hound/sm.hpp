ef5fc82bc93b06c45376d55c40626a1c26c578df
Tom Schmitz <thomas.schmitz98@outlook.com>
2025-11-05 18:33:36 -0500

    Start building initial implementation

--- ares-iq-extensions/include/ares-iq/signal-hound/sm.hpp
+++ ares-iq-extensions/include/ares-iq/signal-hound/sm.hpp
@@ -5,6 +5,10 @@
 #ifndef VERSION_SM_HPP
 #define VERSION_SM_HPP
 
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
 struct SMConfigs {
 
 };
@@ -14,8 +18,13 @@ public:
     explicit SM(const SMConfigs &configs);
     ~SM();
 
+    py::tuple capture_iq(double center, double bw, double file_size_gb,
+                         bool verbose, bool extra);
+
 private:
-    int fd;
+    int fd = -1;
+    SMConfigs _configs;
+    bool _open = false;
 };
 
 #endif //VERSION_SM_HPP
--- ares-iq-extensions/src/signal-hound/sm_series.cpp
+++ ares-iq-extensions/src/signal-hound/sm_series.cpp
@@ -11,3 +11,19 @@ namespace py = pybind11;
 PYBIND11_MODULE(_sh_sm_series, m, py::mod_gil_not_used()) {
     m.def("sm_api_version", smGetAPIVersion, "Retrieve the SM API version");
 }
+
+SM::SM(const SMConfigs &configs) {
+    _configs = configs;
+}
+
+SM::~SM() {
+    if (_open) {
+        smCloseDevice(fd);
+    }
+}
+
+py::tuple SM::capture_iq(double center, double bw, double file_size_gb,
+    bool verbose, bool extra) {
+
+    return py::make_tuple();
+}
