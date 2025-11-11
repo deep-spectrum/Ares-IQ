d92cdffba840591f14c58dc961e39943470a3d6f
Tom Schmitz <thomas.schmitz98@outlook.com>
2025-11-11 17:52:29 -0500

    Review fixes

--- ares-iq-extensions/cmake/AddPythonModule.cmake
+++ ares-iq-extensions/cmake/AddPythonModule.cmake
@@ -1,32 +1,30 @@
-if(APPLE)
-    set(origin_token "@loader_path")
-else()
-    set(origin_token "$ORIGIN")
-endif()
-
 #[[
-  python_module(<name> <src_dir>
+  python_module(<name> <src>...
       [DESTINATION <path>]
       [DEPENDENCIES <target>...]
       [LIBS <lib>...]
       [DEFINITIONS <def>...]
-      [INSTALL_LIBS <file>...])
+      [INSTALL_LIBS <file>...]
+      [PYBIND_LIBS <lib>...])
   Defines and installs a Python C++ extension module.
 
   Arguments:
     <name>         – Target name for the module.
-    <src_dir>      – Path to the .cpp file for pybind11.
+    <src>          – Path to the .cpp files for pybind11.
     DESTINATION    – Install path (relative to prefix).
     DEPENDENCIES   – Other targets this depends on.
     LIBS           – Libraries to link against.
     DEFINITIONS    – Preprocessor definitions.
     INSTALL_LIBS   – Shared libraries to install with the module.
+    PYBIND_LIBS    - Pybind11 libraries to link to. Defaults to pybind11::headers if not specified.
+
+  Requires CMake >= 3.15
 ]]
 function(python_module name src_dir)
     # Required arguments
     set(options)
     set(one_value_args DESTINATION)
-    set(multi_value_args DEPENDENCIES LIBS DEFINITIONS INSTALL_LIBS)
+    set(multi_value_args DEPENDENCIES LIBS DEFINITIONS INSTALL_LIBS PYBIND_LIBS)
     cmake_parse_arguments(MOD "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
 
     # Create Python module
@@ -37,19 +35,33 @@ function(python_module name src_dir)
         add_dependencies(${name} ${MOD_DEPENDENCIES})
     endif()
 
+    if (NOT MOD_PYBIND_LIBS)
+        set(MOD_PYBIND_LIBS pybind11::headers)
+    endif()
+
     # Link libraries
-    target_link_libraries(${name} PRIVATE pybind11::headers ${MOD_LIBS})
+    target_link_libraries(${name} PRIVATE ${MOD_PYBIND_LIBS} ${MOD_LIBS})
 
     # Add compile definitions
     if(MOD_DEFINITIONS)
         target_compile_definitions(${name} PRIVATE ${MOD_DEFINITIONS})
     endif()
 
+    if (NOT MOD_DESTINATION)
+        message(FATAL_ERROR "python_module() DESTINATION is required")
+    endif ()
+
     # Install target
     install(TARGETS ${name} DESTINATION ${MOD_DESTINATION})
 
     # Optional library installs (e.g., shared libs)
     if(MOD_INSTALL_LIBS)
+        foreach (lib ${MOD_INSTALL_LIBS})
+            if (NOT EXISTS "${lib}")
+                message(FATAL_ERROR "${lib} does not exist")
+            endif ()
+        endforeach ()
+
         install(FILES ${MOD_INSTALL_LIBS} DESTINATION ${MOD_DESTINATION}/lib)
         if(APPLE)
             set(origin_token "@loader_path")
@@ -65,16 +77,23 @@ endfunction()
   Installs a Python package given the relative path to the package.
 ]]
 function(python_package package_src_dir)
+    set(options)
+    set(one_value_args DESTINATION)
+    set(multi_value_args)
+    cmake_parse_arguments(PKG "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
+
     get_filename_component(abs_src_dir ${package_src_dir} ABSOLUTE)
     if(NOT IS_DIRECTORY ${abs_src_dir})
         message(FATAL_ERROR "install_python_package: '${package_src_dir}' is not a valid directory")
     endif()
 
-    file(RELATIVE_PATH rel_install_path ${CMAKE_SOURCE_DIR}/src ${abs_src_dir})
+    if (NOT PKG_DESTINATION)
+        message(FATAL_ERROR "python_package() DESTINATION is required")
+    endif ()
 
     install(
             DIRECTORY ${abs_src_dir}/
-            DESTINATION ${rel_install_path}
+            DESTINATION ${PKG_DESTINATION}
             FILES_MATCHING
             PATTERN "*.py"
     )
