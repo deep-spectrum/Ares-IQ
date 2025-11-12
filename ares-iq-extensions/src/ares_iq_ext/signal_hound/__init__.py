b799767279b99b9852f737e1c0986a7a7bec9340
Tom Schmitz <thomas.schmitz98@outlook.com>
2025-11-12 17:39:26 -0500

    Now checking if libusb is installed and raising error with helpful message if it is not

--- ares-iq-extensions/src/ares_iq_ext/signal_hound/__init__.py
+++ ares-iq-extensions/src/ares_iq_ext/signal_hound/__init__.py
@@ -1,4 +1,8 @@
 from __future__ import annotations
+import ctypes.util
+
+if not ctypes.util.find_library('usb-1.0'):
+    raise OSError('Missing libusb-1.0-0. Please run `sudo apt install libusb-1.0-0`')
 
 from ._sh_sm_series import sm_api_version, SmDeviceType, _SmDevice, get_device_list, get_device_list2
 
