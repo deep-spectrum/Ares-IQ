587a8f677d783302896d4ea7e265566624fc2f49
Tom Schmitz <thomas.schmitz98@outlook.com>
2025-11-21 14:01:47 -0500

    Add new attribute to init file

--- ares-iq-extensions/src/ares_iq_ext/signal_hound/__init__.py
+++ ares-iq-extensions/src/ares_iq_ext/signal_hound/__init__.py
@@ -5,7 +5,7 @@ if not ctypes.util.find_library('usb-1.0'):
     raise OSError('Missing libusb-1.0-0. Please run `sudo apt install libusb-1.0-0`')
 
 from ._sh_sm_series import sm_api_version, SmDeviceType, SmGpsPlatformModel, _SmConfigs, _SmDevice, _SM, \
-    get_device_list, get_device_list2, HOST_ADDR_ANY, DEFAULT_DEV_ADDR, DEFAULT_PORT
+    get_device_list, get_device_list2, HOST_ADDR_ANY, DEFAULT_DEV_ADDR, DEFAULT_PORT, SM_MAX_IQ_DECIMATION
 from ._sh_sm_series import LOGGER_NAME as SM_LOGGER_NAME
 
 __all__ = [
@@ -28,4 +28,5 @@ __all__ = [
     "DEFAULT_DEV_ADDR",
     "DEFAULT_PORT",
     "SM_LOGGER_NAME",
+    "SM_MAX_IQ_DECIMATION",
 ]
