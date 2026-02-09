518abe8ce686010bf859357615df52d786323276
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-02-09 13:43:02 -0500

    Add sm series to cli tool

--- src/ares_iq/app/signal_hound/__init__.py
+++ src/ares_iq/app/signal_hound/__init__.py
@@ -2,7 +2,13 @@ from ares_iq.typing import SoftwareDefinedRadio
 from typing import Type
 
 from .bb60 import BB60Device
+from .sm_series import SMDevice
 
 PLATFORMS: dict[str, Type[SoftwareDefinedRadio]] = {
     "bb60": BB60Device,
+    "sm200a": SMDevice,
+    "sm200b": SMDevice,
+    "sm200c": SMDevice,
+    "sm435b": SMDevice,
+    "sm435c": SMDevice,
 }
