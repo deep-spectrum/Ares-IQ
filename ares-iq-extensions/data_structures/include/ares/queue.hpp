7dc46bfbab2f1f92a1ac32749966ec45b206a0b4
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-02-17 17:18:09 -0500

    Add library for data structures

---
+++ ares-iq-extensions/data_structures/include/ares/queue.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file queue.hpp
+ *
+ * @brief
+ *
+ * @date 2/17/26
+ *
+ * @author Tom Schmitz \<tschmitz@andrew.cmu.edu\>
+ */
+
+
+#ifndef ARES_QUEUE_HPP
+#define ARES_QUEUE_HPP
+
+#endif //ARES_QUEUE_HPP
---
+++ ares-iq-extensions/data_structures/src/ares/queue.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file queue.cpp
+ *
+ * @brief
+ *
+ * @date 2/17/26
+ *
+ * @author Tom Schmitz \<tschmitz@andrew.cmu.edu\>
+ */
