a2250c183c1b12703d4c4cf3e9d18faa424e8e87
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-02-23 21:28:26 -0500

    Revert

--- ares-iq-extensions/capture_progress/src/capture-progress/progress.cpp
+++ ares-iq-extensions/capture_progress/src/capture-progress/progress.cpp
@@ -235,7 +235,7 @@ void Progress::_draw_bar() const {
 
     std::cout << RichRgb(current_progress_color, utf8_complete_bar)
               << RichRgb(remaining_progress_color, utf8_remaining_bar) << " "
-              << RichMagenta(static_cast<int>(_percent_complete * 100.0), "%  ");
+              << RichMagenta(static_cast<int>(_percent_complete * 100.0), "% ");
 }
 
 void Progress::_draw_rate() const { std::cout << RichMagenta(_rate); }
