1daaa441616c1bdf0143eb41e9247b7db6377022
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-02-25 13:13:35 -0500

    Running a test

--- ares-iq-extensions/capture_progress/src/capture-progress/monitor.cpp
+++ ares-iq-extensions/capture_progress/src/capture-progress/monitor.cpp
@@ -103,7 +103,7 @@ void MemoryMonitor::_draw(const Memory &mem) const {
 }
 
 void MemoryMonitor::_draw_opening() {
-    std::cout << RichCyan(FontStyle::FONT_BOLD, "Mem")
+    std::cout << RichCyan(FontStyle::FONT_BOLD | FontStyle::FONT_UNDERLINE | FontStyle::FONT_STRIKETHROUGH, "Mem")
               << RichWhite(FontStyle::FONT_BOLD, "[");
 }
 
