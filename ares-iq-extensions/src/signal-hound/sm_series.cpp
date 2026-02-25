2fa12e7542eb165e396b7347f412fbad11aeee74
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-02-25 16:13:39 -0500

    Add error checking

--- ares-iq-extensions/src/signal-hound/sm_series.cpp
+++ ares-iq-extensions/src/signal-hound/sm_series.cpp
@@ -810,21 +810,32 @@ bool SM::_capture_iq_data(uint64_t captures, ares::queue<RawCapture *> &queue,
     int sample_loss;
     bool sample_loss_ = false;
     uint32_t samples_per_capture = _configs.samples_per_capture;
+    int32_t error_break_condition = 100;
 
-    for (size_t i = 0; i < captures; i++) {
+    SmStatus iq_status, gps_status;
+    for (size_t i = 0; i < captures && error_break_condition > 0; i++) {
         auto *capture = new RawCapture();
         capture->buf.resize(samples_per_capture * 2);
 
-        smGetIQ(fd, capture->buf.data(), static_cast<int>(samples_per_capture),
-                nullptr, 0, &capture->timestamp, smFalse, &sample_loss,
-                nullptr);
-        smGetGPSInfo(fd, smFalse, nullptr, &capture->gps_info.sec_since_epoch,
-                     &capture->gps_info.latitude, &capture->gps_info.longitude,
-                     &capture->gps_info.altitude, nullptr, nullptr);
+        iq_status = smGetIQ(
+            fd, capture->buf.data(), static_cast<int>(samples_per_capture),
+            nullptr, 0, &capture->timestamp, smFalse, &sample_loss, nullptr);
+        gps_status = smGetGPSInfo(
+            fd, smFalse, nullptr, &capture->gps_info.sec_since_epoch,
+            &capture->gps_info.latitude, &capture->gps_info.longitude,
+            &capture->gps_info.altitude, nullptr, nullptr);
+        if (iq_status != smNoError || gps_status != smNoError) {
+            LOG_WRN("I/Q Error: %s, GPS Error: %s", smGetErrorString(iq_status),
+                    smGetErrorString(gps_status));
+            delete capture;
+            sample_loss_ = true;
+            error_break_condition--;
+            continue;
+        }
         capture->chunk_id = chunk;
         queue.put(capture);
         if (sample_loss == SM_TRUE) {
-            LOG_WRN("SM APi dropping samples");
+            LOG_WRN("SM API dropping samples");
             sample_loss_ = true;
         }
     }
