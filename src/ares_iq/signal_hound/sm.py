dbde49554c5a2f7b737712057994d2b389501c59
Tom Schmitz <thomas.schmitz98@outlook.com>
2026-06-04 14:14:05 -0400

    Always aborting measurement when done

--- src/ares_iq/signal_hound/sm.py
+++ src/ares_iq/signal_hound/sm.py
@@ -394,6 +394,9 @@ class SM(ABC):
             meta = self._dev.stream_iq(params)
         except _SmException as e:
             exception = SmException(e)
+        except Exception as e:
+            exception = e
+
         if exception is not None:
             self.abort_measurement()
             raise exception
